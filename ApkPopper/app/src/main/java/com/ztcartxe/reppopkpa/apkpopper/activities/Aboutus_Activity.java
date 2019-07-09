236236956d1a7e38da573da4d4bf2cc97d19dc3c
suz <suz_47@outlook.com>
2019-07-10 02:11:10 +0545

    Added License
    Added License Detail in each .java file

--- ApkPopper/app/src/main/java/com/ztcartxe/reppopkpa/apkpopper/activities/Aboutus_Activity.java
+++ ApkPopper/app/src/main/java/com/ztcartxe/reppopkpa/apkpopper/activities/Aboutus_Activity.java
@@ -1,3 +1,24 @@
+/*
+    <one line to give the program's name and a brief idea of what it does.>
+    ApkPopper "Simple App that lets user to get info about installed app and extract them."
+    Copyright (C) 2019  Sujan Thapa
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+ */
+
+
 package com.ztcartxe.reppopkpa.apkpopper.activities;
 
 import androidx.annotation.NonNull;
@@ -43,7 +64,7 @@ public class Aboutus_Activity extends AppCompatActivity {
             startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.gnu.org/licenses/gpl-3.0.en.html")));
         }
         else if(view.getId() == R.id.sourceCodeGithubBtn){
-            startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/bleedweedsuz/AndroidEbookViewer")));
+            startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/bleedweedsuz/ApkPopper")));
         }
     }
 }
--- ApkPopper/app/src/main/java/com/ztcartxe/reppopkpa/apkpopper/model/PackageInfoItem.java
+++ ApkPopper/app/src/main/java/com/ztcartxe/reppopkpa/apkpopper/model/PackageInfoItem.java
@@ -1,3 +1,22 @@
+/*
+    <one line to give the program's name and a brief idea of what it does.>
+    ApkPopper "Simple App that lets user to get info about installed app and extract them."
+    Copyright (C) 2019  Sujan Thapa
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+ */
 package com.ztcartxe.reppopkpa.apkpopper.model;
 
 import android.content.Context;
